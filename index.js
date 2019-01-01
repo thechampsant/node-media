95f37d385ac2baee4ddd74c1aeb0d46aa9b4ff54
Santosh Kumar Prasad <santoshprsd71@gmail.com>
2019-01-01 18:41:53 +0530

    Heroku Config

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var cloudinary = require('cloudinary')
-cloudinary.uploader.upload("logo.png",
-function(result) { console.log(result) });
+var cloudinary = require('cloudinary').v2;
+
+
+cloudinary.config({ 
+  cloud_name: 'cloudinary', 
+  api_key: '228326894266236', 
+  api_secret: 'an_9usvzw_s-DTdnT2Hzb1ypank@hre2ge7zv' 
+});
+
+cloudinary.uploader.upload("./page1.jpg",(err)=>{
+    if(err)
+        return console.log(err);
+});
